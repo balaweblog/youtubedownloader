09f8ad4321c215e50ab7444b5b969bd40b7e774d
Balamurugan Alagumalai <balamurugan.alagumalai@cognizant.com>
2019-08-04 20:08:43 +0530

    push

--- videodownloader.py
+++ videodownloader.py
@@ -18,3 +18,4 @@ mp3_playlist_options = {
 playlist ='https://www.youtube.com/playlist?list=PLloxhkoIi9rDjzSsVNW8QA23l7HvyczFz'
 with youtube_dl.YoutubeDL(mp3_playlist_options) as ydl:
     ydl.download([playlist])
+
